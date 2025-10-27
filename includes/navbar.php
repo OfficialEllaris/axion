316accb5940808c9edaa45798fda53cde428b869
Excel Oghenetejiri Prosper <official.ellaris@gmail.com>
2025-10-27 12:10:52 +0100

    Add initial HTML structure for About, Contact, Login, Onboarding, Pricing pages and Navbar

---
+++ includes/navbar.php
@@ -0,0 +1,26 @@
+<nav class="navbar navbar-expand-lg bg-body-tertiary my-4 rounded-4">
+    <div class="container-fluid">
+        <a class="navbar-brand fw-medium pt-0 fs-2 text-success" href="./">Axion.</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="./about.php">About</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="./pricing.php">Pricing</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="./contact.php">Contact</a>
+                </li>
+            </ul>
+            <div class="d-flex gap-2">
+                <a href="./login.php" class="btn btn-success rounded-3">Login</a>
+                <a href="./onboarding.php" class="btn btn-outline-success rounded-3">Onboarding</a>
+            </div>
+        </div>
+    </div>
+</nav>
